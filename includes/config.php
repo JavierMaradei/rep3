419230cd495a6bbff4faea9f245ed4161cb8c587
Javier Maradei <javier.maradei@hotmail.com>
2022-07-08 10:59:55 -0300

    config

--- includes/config.php
+++ includes/config.php
@@ -11,9 +11,9 @@ switch ($mode) {
         define("MODO", 'dev');
     break;
     case 'prod':
-        define("DB_DSN", 'mysql:dbname=rep3;host=localhost;charset=UTF8');
-        define("DB_USER", 'root');
-        define("DB_PASS", '');
+        define("DB_DSN", 'mysql:dbname=gufaxrex_rep3;host=localhost;charset=UTF8');
+        define("DB_USER", 'gufaxrex_rep3');
+        define("DB_PASS", '=b5Dp!T7$}QZ');
         define("MODO", 'prod');
     break;
 }
